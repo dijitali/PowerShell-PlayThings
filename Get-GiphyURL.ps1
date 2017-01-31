ce6b311a5e1c0ef127080216d4deab50b9ccb0ed
Ieuan Jenkins <dijitali@users.noreply.github.com>
2017-01-31 15:20:39 +0000

    Added string sanitisation and basic error handling

--- Get-GiphyURL.ps1
+++ Get-GiphyURL.ps1
@@ -2,11 +2,21 @@
 {
     Param
     (
+        [Parameter(Mandatory=$True)] 
         [string] $SearchString = 'funny+cat'
     )
+     $stringMatch = $SearchString | Select-String -Pattern '[a-zA-Z0-9]+' -AllMatches
 
-    $giphyResponse = Invoke-RestMethod -Method Get -Uri ('http://api.giphy.com/v1/gifs/search?q=' + $SearchString + '&api_key=dc6zaTOxFJmzC')
-
+    if($stringMatch)
+    {
+        $sanitisedSearchString = $stringMatch.Matches.Value -join '+'
+    }
+    else
+    {
+        throw ('Invalid SearchString: ' + $SearchString)
+    } 
+    
+    $giphyResponse = Invoke-RestMethod -Method Get -Uri ('http://api.giphy.com/v1/gifs/search?q=' + $sanitisedSearchString + '&api_key=dc6zaTOxFJmzC')  -ErrorAction Stop
     $index = Get-Random -Minimum 0 -Maximum $giphyResponse.data.Length
     
     return $giphyResponse.data[$index].images.original.url
